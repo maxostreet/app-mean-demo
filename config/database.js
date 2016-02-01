8eb1ce4dccb43d06a3c1bd2a1b9bd3d159999f74
Max <max@ostreet.org>
2016-01-31 22:12:03 -0500

    try second

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/app-demo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+//	'url' : 'mongodb://localhost:27017/app-demo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'maxhuang:iwanttofly@ds053295.mongolab.com:53295/app-demo'
 };
